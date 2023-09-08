88642e319aa8cb93aea40547063c67f1762be183
dong5397 <dongwok5397@naver.com>
2023-09-08 20:59:11 +0900

    class만들기

---
+++ script.js
@@ -0,0 +1,10 @@
+class Calculator {}
+const cal = new Calculator();
+
+const $previousPreview = document.querySelector("[data-previous-preview]");
+const $currentPreview = document.querySelector("[data-current-preview]");
+console.log($previousPreview);
+console.log($currentPreview);
+
+// function Calculator () {}
+// const cal = new Calculator()
