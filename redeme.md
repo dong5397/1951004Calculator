3e592f4e19c93121f62dee9a289ebc54ca9b4d13
dong5397 <dongwok5397@naver.com>
2023-09-13 17:38:12 +0900

    1

--- redeme.md
+++ redeme.md
@@ -2,10 +2,10 @@
 
 ### 1주차
 
-- [ ] 기본 UI 완성
-- [ ] data 속성 추가
-- [ ] calculator class 만들기
-- [ ] 계산기 기능 관련 DOM 셋팅
+- [x] 기본 UI 완성
+- [x] data 속성 추가
+- [x] calculator class 만들기
+- [x] 계산기 기능 관련 DOM 셋팅
 - [ ] 숫자 버튼 기능(onPressNumber) 구현
 
 ### 2주차
--- script.js
+++ script.js
@@ -1,10 +1,62 @@
-class Calculator {}
-const cal = new Calculator();
+class Calculator {
+  $previousPreview;
+  $currentPreview;
+  constructor($previousPreview, $currentPreview) {
+    this.$previousPreview = $previousPreview;
+    this.$currentPreview = $currentPreview;
+  }
+  onPressNumber(number) {
+    // TODO: 벨리데이션
+    this.$currentPreview.textContent += number;
+  }
 
+  onPressOperation(operation) {
+    this.$previousPreview.textContent = `${this.$currentPreview.textContent} ${operation}`;
+    this.$currentPreview.textContent = "";
+  }
+}
+
+// 값 표시
 const $previousPreview = document.querySelector("[data-previous-preview]");
 const $currentPreview = document.querySelector("[data-current-preview]");
-console.log($previousPreview);
-console.log($currentPreview);
+// 사칙연산
+const $plus = document.querySelector("[data-btn-plus]");
+const $minus = document.querySelector("[data-btn-minus]");
+const $multiply = document.querySelector("[data-btn-multiply]");
+const $divide = document.querySelector("[data-btn-divide]");
+const $eqaul = document.querySelectorAll("[data-btn-eqaul]");
 
-// function Calculator () {}
-// const cal = new Calculator()
+// 숫자, 연산
+const $numbers = document.querySelectorAll("[data-btn-number]");
+const $operations = document.querySelectorAll("[data-btn-operation]");
+const cal = new Calculator($previousPreview, $currentPreview);
+$numbers.forEach(($number) => {
+  $number.addEventListener("click", (e) => {
+    cal.onPressNumber(e.target.textContent);
+  });
+});
+
+$operations.forEach(($operation) => {
+  $operation.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+    switch ($operation) {
+      case $plus:
+        cal.onPressOperation("+");
+        break;
+      case $minus:
+        cal.onPressOperation("-");
+        break;
+      case $multiply:
+        cal.onPressOperation("*");
+        break;
+      case $divide:
+        cal.onPressOperation("÷");
+        break;
+      case $eqaul:
+        //
+        break;
+      default:
+        break;
+    }
+  });
+});
